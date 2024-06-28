37802135b1f3091b000fc0642a0eec848a5a0750
sina1010anis <sina1010anis@gmail.com>
2024-06-28 19:43:24 +0330

    Shift only

---
+++ ABC086A/Shift only/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$handle = fopen("user.txt", "r");
+
+fscanf($handle, "%d", $n);
+
+$numbers = explode(" ", trim(fgets($handle)));
+
+$n = count($numbers);
+
+$count = 0;
+
+$finsh = true;
+
+while ($finsh) {
+
+    for ($i = 0 ; $i < $n ; $i++) { // Block M1 ( Start )
+
+        $numbers[$i] = (int)$numbers[$i];
+
+        if ($numbers[$i] % 2 == 0) {
+    
+            $numbers[$i] = $numbers[$i] / 2;
+    
+        } else {
+    
+            $finsh = false;
+    
+            break;
+    
+        }
+    
+    } // Block M1 ( End )
+
+    $finsh == false ? false : $count++;
+}
+
+echo $count;
